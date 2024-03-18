bbc9782c83706cfa30b7239cff8456299dcd6cbf
Amaniatiah <amaniatiah@gmail.com>
2024-03-18 20:14:07 +0300

    temprature convert

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,71 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static string tempConvert(double tempValue, string unit)
+    {
+        switch (unit)
+        {
+            case "C":
+                double fahrenheit = (tempValue * 9 / 5) + 32;
+                return $"{fahrenheit} F";
+
+            case "F":
+                double celsius = (tempValue - 32) * 5 / 9;
+                return $"{celsius} C";
+
+            default:
+                return "Invalid unit. Please Enter'C' for Celsius or 'F' for Fahrenheit.";
+        }
+    }
+    public static void Main(string[] args)
+    {
+        try
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a temprature Value and its unit (C or F) or type Quit to exit the app");
+                string input = Console.ReadLine() ?? "";
+
+                if (input.ToLower() == "quit")
+                {
+                    Console.WriteLine("Program is terminated");
+                    break;
+                }
+
+
+                string[] parts = input.Split(" ");
+                if (parts.Length != 2)
+                {
+                    Console.WriteLine("Invalid input . please Enter a temperature Value and its unit (C or F)");
+                    continue;
+                }
+
+
+                string tempUnit = parts[1].ToUpper();
+                if (tempUnit != "C" && tempUnit != "F")
+                {
+                    Console.WriteLine("Invalid unit. please Enter C or F as temperature unit.");
+                    continue;
+                }
+                if (!double.TryParse(parts[0], out double temperatureValue))
+                {
+                    Console.WriteLine("Invalid unit. please Enter C or F as temperature unit.");
+                    continue;
+
+                }
+
+
+                Console.WriteLine($"Converted :  {tempConvert(temperatureValue, tempUnit)}");
+
+
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        Console.ReadKey();
+
+
+    }
+}
